06c27ea1f1eb5e34ec0134d7fb478c0079ddbc07
Thomasevano <thomas-evano@hotmail.fr>
2024-05-09 18:47:00 +0200

    return data from db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+import Fastify from 'fastify'
+import db from './db'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  const players = await db.player.findMany()
+  return players
+})
+
+/**
+ * Run the server!
+ */
+const port = Number(process.env.PORT ?? 3000)
+const start = async () => {
+  try {
+    await fastify.listen({ port, host: '0.0.0.0' })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
